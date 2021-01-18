d5a1ea7b93a889ee2903511836554dfca1d7d07b
karanjhala <karanjhala@gmail.com>
2021-01-18 17:33:30 +0800

    clean up index.js and connect to mongodb atlas

--- index.js
+++ index.js
@@ -8,21 +8,26 @@ const PORT = 4000;
 
 // connect mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
+
+
+//connect mongoose to remote DB (MongoDB Atlas)
+mongoose.connect('mongodb+srv://csvtomongo-service:HlM9w83ykmrjWpSu@cluster0.httcb.mongodb.net/restRecordDB?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+// ALTERNATIVE Connect mongoose locally
+// mongoose.connect('mongodb://localhost/CRMdb', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
 // connect bodyparser
 app.use(bodyParser.urlencoded({extended: true}) );
 app.use(bodyParser.json());
 
-//this line calls the "routes" function and gives it the app parameter
+//call the "routes" function and gives it the app parameter
 routes(app);
-
-app.get('/', (req, res) => {
-    res.send(`Node Server running with Express via ${PORT}`)
-});
 
 app.listen(PORT, () => {
     console.log(`Your Server is running on: ${PORT} port`)
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,6 +1,13 @@
 import {addCsvToMongo, getReport } from '../controllers/crmController'
 
+const PORT = 4000;
+
 const routes = (app) => {
+    app.get('/', (req, res) => {
+        res.send(`Node Server running with Express via Port: ${PORT}, Use the Readme to use different APIs`)
+    });
+    
+    
     app.route('/sales/record')
         .get((req,res,next) => {
             console.log(req.method);
