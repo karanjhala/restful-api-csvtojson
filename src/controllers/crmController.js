b9ffa77c544df845bf86d6a237c40e2f42233e28
karanjhala <karanjhala@gmail.com>
2021-01-18 17:32:25 +0800

    add sales/record csvtojson logic

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -1,12 +1,36 @@
 import mongoose from 'mongoose';
 import {SalesRecordSchema} from '../models/crmModel';
-const csvtojson = require("csvtojson");
+import csvtojson from 'csvtojson';
 import request from 'request';
 
 const Record = mongoose.model('SalesRecord', SalesRecordSchema);
-let url = "mongodb://localhost:27017/";
 
-//controller for sales/report
+/***************************controller for sales/record***************************/
+export const addCsvToMongo = (req,res) => {
+    const defaultUrl = "https://storage.googleapis.com/csvstorenodeapp/MOCK_DATA-1.csv"
+
+    csvtojson()
+        // request CSV from a URL
+        .fromStream(request.get(req.query.csvUrl || defaultUrl))
+        // subscribe and feed data to MongoDB
+        .subscribe((csvData, lineNum) => {
+            return new Promise((resolve,reject)=>{
+                
+                let newRecord = Record(csvData);
+                // save row in MongoDB collection
+                newRecord.save((err, record) => {
+                    if (err) {
+                    console.error(err)
+                        reject();
+                    }
+                    console.log(record);
+                    resolve();
+                });
+            });
+});
+}
+
+/***************************controller for sales/report***************************/
 export const getReport = (req,res) => {
     const dateFrom = req.query.dateOne;
     const dateTo = req.query.dateTwo;
@@ -20,6 +44,7 @@ export const getReport = (req,res) => {
             res.json(record);
         })
       } 
+
       // get all records for specific date
       else if (dateFrom && !dateTo) {
         Record.find({
@@ -32,7 +57,8 @@ export const getReport = (req,res) => {
                 res.json(record);
             })
       }
-      //  get all records for speicific date range
+
+      // get all records for speicific date range
       else if (dateFrom && dateTo) {
       Record.find({
         LAST_PURCHASE_DATE: { 
