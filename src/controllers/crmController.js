a874916459813e3033c5a3c65fdca0878d6582a0
karanjhala <karanjhala@gmail.com>
2021-01-18 12:24:11 +0800

    add logic for sales/report endpoint

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -1,55 +1,49 @@
 import mongoose from 'mongoose';
 import {SalesRecordSchema} from '../models/crmModel';
+const csvtojson = require("csvtojson");
+import request from 'request';
 
 const Record = mongoose.model('SalesRecord', SalesRecordSchema);
+let url = "mongodb://localhost:27017/";
 
-export const addNewRecord = (req,res) => {
-    let newRecord = new Record(req.body);
+//controller for sales/report
+export const getReport = (req,res) => {
+    const dateFrom = req.query.dateOne;
+    const dateTo = req.query.dateTwo;
 
-    newRecord.save((err,record) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(record);
-    })
-}
+    // get all records when no dates specified
+    if (!dateFrom && !dateTo) {
+        Record.find({}, (err,record) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(record);
+        })
+      } 
+      // get all records for specific date
+      else if (dateFrom && !dateTo) {
+        Record.find({
+            LAST_PURCHASE_DATE: { 
+                $eq: new Date(dateFrom)
+            }}, (err,record) => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json(record);
+            })
+      }
+      //  get all records for speicific date range
+      else if (dateFrom && dateTo) {
+      Record.find({
+        LAST_PURCHASE_DATE: { 
+            $gte: new Date(dateFrom),
+            $lte: new Date(dateTo)
+        }}, (err,record) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(record);
+        })
+    }
 
-export const getRecords = (req,res) => {
-
-    Record.find({},(err,record) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(record);
-    })
-}
-
-export const getRecordById = (req,res) => {
-
-    Record.findById(req.params.recordID,(err,record) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(record);
-    })
-}
-
-export const updateRecord = (req,res) => {
-
-    Record.findById({_id: req.params.recordID},req.body, {new: true, useFindAndModify: true},(err,record) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(record);
-    })
-}
-
-export const deleteRecord = (req,res) => {
-
-    Record.remove({_id: req.params.recordID},(err,record) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({message: "Record Deleted"});
-    })
 }
